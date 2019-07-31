ddf4bab597f14c7f43f148dc3e1ee13db71c00cb
Steffen Mogensen <silverbaq@Steffens-MacBook-Pro.local>
2019-07-31 07:46:24 +0200

    applied org.mockito:mockito-inline

--- src/test/kotlin/VideoSorterTest.kt
+++ src/test/kotlin/VideoSorterTest.kt
@@ -1,6 +1,7 @@
 import dk.w4.downloadsorter.VideoSorter
 import dk.w4.downloadsorter.controllers.DBController
 import dk.w4.downloadsorter.controllers.FileController
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
 
@@ -14,19 +15,19 @@ class VideoSorterTest  {
     @Test
     fun `Can provide a list of new files in a folder`(){
         //TODO: implement test
-        assert(true)
+        assertTrue(true)
     }
 
     @Test
     fun `Can tell is a video is a movie or tv-show`(){
         //TODO: implement test
-        assert(true)
+        assertTrue(true)
     }
 
     @Test
     fun `Can define a files category`(){
         //TODO: implement test
-        assert(true)
+        assertTrue(true)
     }
 
 
