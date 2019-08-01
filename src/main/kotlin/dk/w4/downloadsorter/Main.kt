a09bf976fd6bac42814d00425f6db28980a34dd7
Steffen Mogensen <silverbaq@Steffens-MacBook-Pro.local>
2019-08-01 12:28:05 +0200

    added forever loop, with 5 min pause

--- src/main/kotlin/dk/w4/downloadsorter/Main.kt
+++ src/main/kotlin/dk/w4/downloadsorter/Main.kt
@@ -2,6 +2,7 @@ package dk.w4.downloadsorter
 
 import dk.w4.downloadsorter.controllers.DBController
 import dk.w4.downloadsorter.controllers.FileController
+import java.lang.Thread.sleep
 
 val dbController = DBController()
 val fileController = FileController()
@@ -9,9 +10,13 @@ val fileController = FileController()
 val videoSorter = VideoSorter(dbController, fileController)
 
 fun main(args: Array<String>) {
-    println("Starting sequence")
-    val files = videoSorter.lookThroughFiles()
-    println("Found files: " + files.size + " files")
-    videoSorter.analyzeFiles(files)
-    println("It has been done!!!")
+    while (true) {
+        println("Starting sequence")
+        val files = videoSorter.lookThroughFiles()
+        println("Found files: " + files.size + " files")
+        videoSorter.analyzeFiles(files)
+        println("It has been done!!!")
+        println("Sleeping for 5 minutes.")
+        sleep(1000 * 60 * 5)
+    }
 }
